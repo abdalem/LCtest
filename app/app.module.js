82da871bd5f707265acbe7215b48c04e29e54fbd
abdalem <abdennour.alem@gmail.com>
2017-10-01 13:52:37 +0200

    inject homeState to app

--- app/app.module.js
+++ app/app.module.js
@@ -3,7 +3,8 @@ import uirouter from '@uirouter/angularjs'
 
 import routes from './app.routes'
 import apiService from './services/api.service'
+import homeState from './states/home/home.module'
 
-angular.module('lcTest', [uirouter])
+angular.module('lcTest', [uirouter, homeState])
   .config(routes)
   .service('apiService', apiService)
