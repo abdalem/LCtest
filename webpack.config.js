72e663bd93386104640ae77cd936ef248e64c501
abdalem <abdennour.alem@gmail.com>
2017-10-01 13:13:14 +0200

    add webpack conf

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path')
+
+module.exports = {
+    entry: './app/app.module.js',
+    output: {
+      path: path.resolve("./bin"),
+        filename: 'app.bundle.js',
+    },
+    module: {
+        loaders: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+        },
+        { test: /\.html$/, loader: "html-loader" },
+        { test: /\.scss$/, loader: "style!css!scss" }
+      ]
+    },
+    devtool: "#inline-source-map"
+}
