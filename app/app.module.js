0c5141c8d85f2c0713fee7bd2e80c439c2ef36be
abd <aalem@groupeavantages.fr>
2017-10-02 16:43:16 +0200

    add theming to global module

--- app/app.module.js
+++ app/app.module.js
@@ -5,9 +5,11 @@ import '../node_modules/angular-material/angular-material.scss'
 
 import './assets/styles.scss'
 import routes from './app.routes'
+import theming from './app.theming'
 import apiService from './services/api.service'
-import homeState from './states/home/home.module'
+import homeState from './components/home/home.module'
 
 angular.module('lcTest', [uirouter, homeState])
   .config(routes)
+  .config(theming)
   .service('apiService', apiService)
