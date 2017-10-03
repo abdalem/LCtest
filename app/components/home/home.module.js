8f83e0f50391d658de6e7fa2f0f464071ecca4c2
abd <aalem@groupeavantages.fr>
2017-10-03 11:57:03 +0200

    add pagination module

--- app/components/home/home.module.js
+++ app/components/home/home.module.js
@@ -4,8 +4,10 @@ import angularmd from 'angular-material'
 import './home.scss'
 import routes from './home.routes'
 import homeController from './home.controller'
+import lcPagination from '../lc-pagination/lc-pagination.component'
 
 export default angular.module('lcTest.home', [uirouter, angularmd])
   .config(routes)
   .controller('homeController', homeController)
+  .component('lcPagination', lcPagination)
   .name
---
+++ app/components/lc-pagination/lc-pagination.component.js
@@ -0,0 +1,18 @@
+const lcPaginationCtrl = class {
+  constructor(
+
+  ) {
+
+  }
+
+
+}
+
+module.exports = {
+  template: require('./lc-pagination.html'),
+  controller: lcPaginationCtrl,
+  controllerAs: '$ctrl',
+  bindings: {
+    pokemons: '='
+  }
+}
