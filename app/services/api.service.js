10eb3b767999a2292e0c3677d08ff671a580d85d
abd <aalem@groupeavantages.fr>
2017-10-03 09:54:11 +0200

    add all necessary api methods

--- app/services/api.service.js
+++ app/services/api.service.js
@@ -14,6 +14,60 @@ export default class apiService {
       method: 'GET'
     })
   }
+
+  /**
+  * Create a new product
+  */
+  addProduct(product){
+    return this.$http({
+      url: 'https://test-recrutement.loyaltyexpert.net/products',
+      method: 'POST',
+      data: {
+        name: product.anme,
+        description: product.description,
+        url: product.url,
+        categories: product.categories,
+        brand: product.brand
+      }
+    })
+  }
+
+  /**
+  * Update a product
+  */
+  updateProduct(product){
+    return this.$http({
+      url: 'https://test-recrutement.loyaltyexpert.net/products/' + product.id,
+      method: 'POST',
+      data: {
+        name: product.anme,
+        description: product.description,
+        url: product.url,
+        categories: product.categories,
+        brand: product.brand
+      }
+    })
+  }
+
+  /**
+  * Get brands' list
+  */
+  getBrands(){
+    return this.$http({
+      url: 'https://test-recrutement.loyaltyexpert.net/brands',
+      method: 'GET'
+    })
+  }
+
+  /**
+  * Get categories' list
+  */
+  getCategories(){
+    return this.$http({
+      url: 'https://test-recrutement.loyaltyexpert.net/categories',
+      method: 'GET'
+    })
+  }
 }
 
 apiService.$inject = ['$http']
