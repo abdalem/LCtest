d6f526d6d0e7780e5e9734272f8c168c33316063
abd <aalem@groupeavantages.fr>
2017-10-03 13:07:33 +0200

    add mdDialogs controllers and views

---
+++ app/components/home/add-product/add-product.controller.js
@@ -0,0 +1,7 @@
+export default class addProductCtrl {
+  constructor(
+  ) {
+  }
+}
+
+addProductCtrl.$inject = []
---
+++ app/components/home/update-product/update-product.controller.js
@@ -0,0 +1,7 @@
+export default class updateProductCtrl {
+  constructor(
+  ) {
+  }
+}
+
+updateProductCtrl.$inject = []
