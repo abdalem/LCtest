8f1934d173d00e019dee0fc0932f58fd7cf42b61
abd <aalem@groupeavantages.fr>
2017-10-04 10:24:33 +0200

    improve pagination component

--- app/components/lc-pagination/lc-pagination.component.js
+++ app/components/lc-pagination/lc-pagination.component.js
@@ -3,17 +3,33 @@ import './lc-pagination.scss'
 
 const lcPaginationCtrl = class {
   constructor(
-
+    $mdMedia
   ) {
-
+    this.$mdMedia = $mdMedia
+    this.curPage = 0
+    this.paginationStart = 0
   }
+
 
   getNumber(pages) {
-    return new Array(pages)
+    return Array.from(Array(pages),(x,i)=>i)
   }
 
+  setCurrentPage(page) {
+    this.setPage({page: page})
+    this.curPage = page
+    this.paginationStart = page<=2?0:(page>=this.pages-2?this.pages-5:page-2)
+
+  }
+
+  getStyle(page) {
+    if (page == this.curPage) {
+      return {'background-color': 'rgb(63,81,181)', 'color': 'white'}
+    }
+  }
 }
 
+lcPaginationCtrl.$inject = ['$mdMedia']
 module.exports = {
   template: require('./lc-pagination.html'),
   controller: lcPaginationCtrl,
